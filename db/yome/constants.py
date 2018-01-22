bcc8ef2861ab99aaa2cbf7795e8e8ecb6fdd6991
Zak King <zaking17@gmail.com>
2018-01-22 15:21:57 -0800

    updated rnaseq fig

---
+++ db/yome/constants.py
@@ -0,0 +1,14 @@
+colors = {
+    'bright red': [ 0.85453713,  0.22957019,  0.27623218,  1.        ], # bright red
+    'red': [ 0.9170408 ,  0.56061475,  0.58722664,  1.        ], # red
+    'light blue green': [ 0.57653009,  0.72175186,  0.76633449,  1.        ], # light blue green
+    'dark blue green': [ 0.24715576,  0.49918708,  0.57655991,  1.        ], # dark blue green
+    'grey': [ 0.6,         0.6,         0.6,         1.        ], # grey
+}
+colors_by_number = {
+    0: colors['bright red'],
+    1: colors['red'],
+    2: colors['light blue green'],
+    3: colors['dark blue green'],
+    -1: colors['grey'],
+}
