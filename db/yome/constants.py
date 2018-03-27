3b3cf12e834213899506a6f52d4b4e374c8efd52
Zak King <zaking17@gmail.com>
2018-03-27 12:35:06 -0700

    new colors & category plots

--- db/yome/constants.py
+++ db/yome/constants.py
@@ -1,14 +1,21 @@
 colors = {
-    'bright red': [ 0.85453713,  0.22957019,  0.27623218,  1.        ], # bright red
-    'red': [ 0.9170408 ,  0.56061475,  0.58722664,  1.        ], # red
-    'light blue green': [ 0.57653009,  0.72175186,  0.76633449,  1.        ], # light blue green
-    'dark blue green': [ 0.24715576,  0.49918708,  0.57655991,  1.        ], # dark blue green
-    'grey': [ 0.6,         0.6,         0.6,         1.        ], # grey
+    'red': '#DD4D42',
+    'yellow': '#F1D333',
+    'green': '#5B9378',
+    'blue': '#4A70BC',
+    'blue grey': '#DEE6F3',
 }
 colors_by_number = {
-    0: colors['bright red'],
-    1: colors['red'],
-    2: colors['light blue green'],
-    3: colors['dark blue green'],
-    -1: colors['grey'],
+    0: colors['red'],
+    1: colors['yellow'],
+    2: colors['green'],
+    3: colors['blue'],
+    -1: colors['blue grey'],
+}
+hatches_by_number = {
+    0: '',    # excluded
+    1: '\\',  # tbd
+    2: '//',  # Y-ome
+    3: '..',  # high
+    -1: '',   # Not present
 }
--- db/yome/util.py
+++ db/yome/util.py
@@ -153,3 +153,40 @@ def report(session, locus_tag):
     features_df = features_df.set_index(['knowledgebase_name', 'feature_type'])
     s = features_df.style.set_properties(**{'text-align': 'left'})
     return HTML(s.render())
+
+
+def clamp(val, minimum=0, maximum=255):
+    if val < minimum:
+        return minimum
+    if val > maximum:
+        return maximum
+    return round(val)
+
+
+def scale_color(hexstr, scalefactor):
+    """Scales a hex string by ``scalefactor``. Returns scaled hex string.
+
+    To darken the color, use a float value between 0 and 1.
+    To brighten the color, use a float value greater than 1.
+
+    >>> colorscale("#DF3C3C", .5)
+    #6F1E1E
+    >>> colorscale("#52D24F", 1.6)
+    #83FF7E
+    >>> colorscale("#4F75D2", 1)
+    #4F75D2
+
+    """
+
+    hexstr = hexstr.strip('#')
+
+    if scalefactor < 0 or len(hexstr) != 6:
+        return hexstr
+
+    r, g, b = int(hexstr[:2], 16), int(hexstr[2:4], 16), int(hexstr[4:], 16)
+
+    r = clamp(r * scalefactor)
+    g = clamp(g * scalefactor)
+    b = clamp(b * scalefactor)
+
+    return '#%02x%02x%02x' % (r, g, b)
