e347fc992ef67b211fac4e84a8f1f99b3e79a0f1
Zachary King <zaking17@gmail.com>
2018-05-03 14:32:08 -0700

    bug fix for binder pytesting

--- yome/util.py
+++ yome/util.py
@@ -194,24 +194,28 @@ def scale_color(hexstr, scalefactor):
 
     return '#%02x%02x%02x' % (r, g, b)
 
-@register_line_magic
-def mpl_setup(line):
-    IPython.get_ipython().magic('pylab --no-import-all inline')
+try:
+    # Only run in the notebook environment
+    @register_line_magic
+    def mpl_setup(line):
+        IPython.get_ipython().magic('pylab --no-import-all inline')
 
-    try:
-        import seaborn as sns
-        sns.set(style="darkgrid")
-    except ImportError:
-        from warnings import warn
-        warn('Seaborn not installed')
+        try:
+            import seaborn as sns
+            sns.set(style="darkgrid")
+        except ImportError:
+            from warnings import warn
+            warn('Seaborn not installed')
 
-    mpl.rcParams['savefig.bbox'] = 'tight'
-    mpl.rcParams['savefig.pad_inches'] = 0
-    mpl.rcParams['text.usetex'] = False
-    mpl.rcParams['lines.linewidth'] = 3
-    mpl.rcParams['font.size'] = 15
-    mpl.rcParams['axes.labelsize'] = 17
-    mpl.rcParams['axes.titlesize'] = 17
-    mpl.rcParams['xtick.labelsize'] = 15
-    mpl.rcParams['ytick.labelsize'] = 15
-    mpl.rcParams['legend.fontsize'] = 17
+        mpl.rcParams['savefig.bbox'] = 'tight'
+        mpl.rcParams['savefig.pad_inches'] = 0
+        mpl.rcParams['text.usetex'] = False
+        mpl.rcParams['lines.linewidth'] = 3
+        mpl.rcParams['font.size'] = 15
+        mpl.rcParams['axes.labelsize'] = 17
+        mpl.rcParams['axes.titlesize'] = 17
+        mpl.rcParams['xtick.labelsize'] = 15
+        mpl.rcParams['ytick.labelsize'] = 15
+        mpl.rcParams['legend.fontsize'] = 17
+except NameError:
+    pass
